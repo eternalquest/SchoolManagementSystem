70560852eac8fd2a360b0bd79e8b4d325b2d30ba
NitishPokhriyal <nitishpokhriyal1@gmail.com>
2023-02-21 19:33:27 +0530

    Update schoolmanagementdb.sql

--- schoolmanagementdb.sql
+++ schoolmanagementdb.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `student_info` (
   `s_id` int unsigned NOT NULL auto_increment,
   `s_name` varchar(500) NOT NULL,
-  `c_id` int unsigned NOT NULL,
+  `class_id` int unsigned NOT NULL,
   `sess_id` int unsigned NOT NULL,
   `roll_no` int unsigned NOT NULL,
   `gender` varchar(45) NOT NULL,
@@ -15,4 +15,24 @@ CREATE TABLE `student_info` (
   `permanent_address` varchar(500) default NULL,
   PRIMARY KEY  (`s_id`)
 
+)
+
+CREATE TABLE `student_attendance` (
+  `sa_id` int unsigned NOT NULL auto_increment,
+  `sa_date` date default NULL,
+  `s_id` int unsigned NOT NULL,
+  `status` varchar(45) NOT NULL default 'A',
+  PRIMARY KEY  (`sa_id`)
+)
+
+CREATE TABLE `class` (
+  `class_id`  unsigned NOT NULL auto_increment,
+  `class_name` varchar(45) NOT NULL,
+  PRIMARY KEY  (`sem_id`)
+) 
+
+CREATE TABLE `subject` (
+  `sub_id` int unsigned NOT NULL auto_increment,
+  `sub_name` varchar(45) NOT NULL,
+  PRIMARY KEY  (`sub_id`)
 )
